51d1b4c3827a55410afd53689429dd0bc7fb1d35
dementr80 <dementr80@gmail.com>
2022-11-24 17:45:44 +0600

    404 page handler was added to back-end

--- back-end/cmd/back-end/main.go
+++ back-end/cmd/back-end/main.go
@@ -6,6 +6,10 @@ import (
 	"golang.org/x/crypto/acme/autocert"
 )
 
+func customHTTPErrorHandler(err error, c echo.Context) {
+	c.File("dist/404.html")
+}
+
 func main() {
 	e := echo.New()
 	e.AutoTLSManager.HostPolicy = autocert.HostWhitelist("dementyanov.ru")
@@ -16,15 +20,16 @@ func main() {
 		AllowMethods: []string{echo.GET},
 	}))
 
-	e.Use(middleware.StaticWithConfig(middleware.StaticConfig{
-		Root:  "dist",
-		Index: "index.html",
-		HTML5: true,
-	}))
+	e.Static("/", "dist")
+	e.Static("/education", "dist")
+	e.Static("/work", "dist")
+	e.Static("/contactme", "dist")
 
 	e.AutoTLSManager.Cache = autocert.DirCache("/opt/portfolio-2022/.certificates")
 	e.Use(middleware.Recover())
 	e.Use(middleware.Logger())
+
+	e.HTTPErrorHandler = customHTTPErrorHandler
 
 	e.Logger.Fatal(e.StartAutoTLS(":443"))
 }
@@ -40,11 +45,12 @@ func main() {
 		AllowMethods: []string{echo.GET, echo.PUT, echo.POST, echo.DELETE},
 	}))
 
-	e.Use(middleware.StaticWithConfig(middleware.StaticConfig{
-		Root:  "dist",
-		Index: "index.html",
-		HTML5: true,
-	}))
+	e.Static("/", "dist")
+	e.Static("/education", "dist")
+	e.Static("/work", "dist")
+	e.Static("/contactme", "dist")
+
+	e.HTTPErrorHandler = customHTTPErrorHandler
 
 	err := e.Start("localhost:8080")
 	if err != nil {
